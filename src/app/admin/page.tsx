96d9a274bdc346806595b019fbdf9b90aaf844c1
Miros987 <mirsichkar@gmail.com>
2025-02-15 14:33:51 +0600

    fixed adaptive2

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -8,25 +8,26 @@ export default function Admin() {
 
     <div className='mt-[70px] flex flex-wrap justify-between  px-[20px] md-0 gap-[20px] text-2xl'>
       <Link href={`/admin/addproduct`}
-        className='w-full md:w-auto  px-[35px] py-[25px] bg-green_first text-white rounded-[10] font-[MullerBold] shadow-[0_0_10px_0_#00000014]'>
+        className='w-full md:w-auto  px-[35px] py-[25px] bg-green_first text-white rounded-[10px] font-[MullerBold] shadow-[0_0_10px_0_#00000014]'>
         Продукция
       </Link>
 
       <Link href={`/admin/crud_category`}
-        className='w-full md:w-auto  px-[35px] py-[25px] bg-green_first text-white  rounded-[10] font-[MullerBold] shadow-[0_0_10px_0_#00000014]'>
+        className='w-full md:w-auto  px-[35px] py-[25px] bg-green_first text-white  rounded-[10px
+        ] font-[MullerBold] shadow-[0_0_10px_0_#00000014]'>
         Категории 
       </Link>
 
       <Link href={``}
-        className='w-full md:w-auto  px-[35px] py-[25px] bg-grey_first rounded-[10] font-[MullerBold] shadow-[0_0_10px_0_#00000014]'>
+        className='w-full md:w-auto  px-[35px] py-[25px] bg-grey_first rounded-[10px] font-[MullerBold] shadow-[0_0_10px_0_#00000014]'>
         Цвета
       </Link>
       <Link href={``}
-        className='w-full md:w-auto px-[35px] py-[25px] bg-grey_first rounded-[10] font-[MullerBold] shadow-[0_0_10px_0_#00000014]'>
+        className='w-full md:w-auto px-[35px] py-[25px] bg-grey_first rounded-[10px] font-[MullerBold] shadow-[0_0_10px_0_#00000014]'>
         Фильтр
       </Link>
       <Link href={``}
-        className='w-full md:w-auto  px-[35px] py-[25px] bg-grey_first rounded-[10] font-[MullerBold] shadow-[0_0_10px_0_#00000014]'>
+        className='w-full md:w-auto  px-[35px] py-[25px] bg-grey_first rounded-[10px] font-[MullerBold] shadow-[0_0_10px_0_#00000014]'>
         Сортировка
       </Link>
 
--- src/app/carddetails/[id]/page.tsx
+++ src/app/carddetails/[id]/page.tsx
@@ -25,13 +25,11 @@ const CardDetails = ({ params }: CardDetailsProps) => {
   const { openModal } = useModal()
   const { GetOneProduct } = useProduct()
   const { AddBasketProduct } = useBasket()
+  const { basket } = useAppSelector(state => state.basket)
   const { userUID } = useAppSelector(state => state.user)
   const { oneProduct, loading } = useAppSelector(state => state.products)
   const { state, handleContent } = useDetailsInfo()
-
   const [typeColor, setTypeColor] = useState<number>(0)
-
-
   useEffect(() => {
     GetOneProduct(newParams?.id);
   }, [newParams?.id]);
@@ -39,6 +37,9 @@ const CardDetails = ({ params }: CardDetailsProps) => {
   useEffect(() => {
     handleContent(<Description description={oneProduct.description} />, false)
   }, [oneProduct])
+
+
+
 
 
 
@@ -142,18 +143,18 @@ const CardDetails = ({ params }: CardDetailsProps) => {
 
                   <div className='flex flex-col gap-[14px] mt-[24px]'>
                     {/* {btnBasket ? */}
-                      <button onClick={()=>basketadd(oneProduct)} className='flex items-center justify-center gap-[10px] border-[1.5px] py-[16px] border rounded-radius border-grey_third font-[MullerBold]'>
+                      {/* <button onClick={()=>basketadd(oneProduct)} className='flex items-center justify-center gap-[10px] border-[1.5px] py-[16px] border rounded-radius border-grey_third font-[MullerBold]'>
                         <svg width="25" height="24" viewBox="0 0 25 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                           <g id="Warning / Circle_Check">
                             <path id="Vector" d="M15.5 10L11.5 14L9.5 12M12.5 21C7.52944 21 3.5 16.9706 3.5 12C3.5 7.02944 7.52944 3 12.5 3C17.4706 3 21.5 7.02944 21.5 12C21.5 16.9706 17.4706 21 12.5 21Z" stroke="#1E2128" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
                           </g>
                         </svg>
                         Товар в корзине!
+                      </button> */}
+                    
+                       <button onClick={() => basketadd(oneProduct)} className='flex items-center justify-center gap-[10px] border-[1.5px] py-[16px] border rounded-radius border-grey_third font-[MullerBold]'>
+                         Добавить в корзину
                       </button>
-                      {/* // :
-                      // <button onClick={() => basketadd(oneProduct)} className='flex items-center justify-center gap-[10px] border-[1.5px] py-[16px] border rounded-radius border-grey_third font-[MullerBold]'>
-                      //   Добавить в корзину
-                      // </button>} */}
 
                     <button className='py-[16px] text-white bg-black rounded-radius  font-[MullerBold]'>
                       Купить сейчас
