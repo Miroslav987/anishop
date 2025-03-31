3a4b4829e78ec5341c42a9bc03286ce6389f7537
Miroslav <mirsichkar@gmail.com>
2025-03-31 19:37:29 +0600

    edite test CEO three

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,14 +5,14 @@ import DetailsInfoProvider from "@/context/CardDetailsInfoProvidre";
 import StoreProvider from "./StoreProvider";
 import NavBar from "@/components/Navbar";
 import Footer from "@/components/Footer";
-import Head from "next/head"; 
+import Head from "next/head";
 
 export const metadata: Metadata = {
   title: "anishop",
-  description: "shop anishop",
+  description: "anishop - интеренет магазин",
   openGraph: {
     title: "anishop",
-    description: "shop anishop",
+    description: "anishop - интеренет магазин",
     url: "https://anishops.vercel.app/",
     images: [
       "https://firebasestorage.googleapis.com/v0/b/onlineshop-4f347.appspot.com/o/logo%2Fanishop.png?alt=media&token=edd7237f-7bf7-4742-ad6c-7b11a81014d0",
@@ -29,12 +29,13 @@ export default function RootLayout({
     <html lang="en">
       <body className="antialiased">
         <Head>
-        <meta
-            name="google-site-verification"
-            content="1m40ZhpkaOBu8WRGiAlr705V3rbGKKkwx9qFUdNcQTE"
-          />
+          <meta property="og:title" content="anishop - Интернет-магазин" />
+          <meta property="og:description" content="anishop - интеренет магазин" />
+          <meta property="og:image" content="https://firebasestorage.googleapis.com/v0/b/onlineshop-4f347.appspot.com/o/logo%2Fanishop.png?alt=media&token=edd7237f-7bf7-4742-ad6c-7b11a81014d0" />
+          <meta property="og:url" content="https://anishops.vercel.app/" />
+          <meta name="google-site-verification" content="1m40ZhpkaOBu8WRGiAlr705V3rbGKKkwx9qFUdNcQTE" />
         </Head>
-        
+
         <StoreProvider>
           <ModalProvider>
             <DetailsInfoProvider>
