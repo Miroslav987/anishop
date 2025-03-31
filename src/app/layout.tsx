a70b03e8ffbb1c48c746ee761723763d6206e96d
Miroslav <mirsichkar@gmail.com>
2025-03-31 19:22:10 +0600

    edit CEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,11 @@
-import type { Metadata } from "next";
+import { Metadata } from "next";
 import "./globals.css";
 import ModalProvider from "@/context/ModalProvider";
 import DetailsInfoProvider from "@/context/CardDetailsInfoProvidre";
 import StoreProvider from "./StoreProvider";
 import NavBar from "@/components/Navbar";
 import Footer from "@/components/Footer";
-
-
+import Head from "next/head"; // Импортируем компонент Head
 
 export const metadata: Metadata = {
   title: "anishop",
@@ -14,30 +13,35 @@ export const metadata: Metadata = {
   openGraph: {
     title: "anishop",
     description: "shop anishop",
-    url:"https://anishops.vercel.app/",
-    images: ["https://firebasestorage.googleapis.com/v0/b/onlineshop-4f347.appspot.com/o/logo%2Fanishop.png?alt=media&token=edd7237f-7bf7-4742-ad6c-7b11a81014d0"],
+    url: "https://anishops.vercel.app/",
+    images: [
+      "https://firebasestorage.googleapis.com/v0/b/onlineshop-4f347.appspot.com/o/logo%2Fanishop.png?alt=media&token=edd7237f-7bf7-4742-ad6c-7b11a81014d0",
+    ],
   },
 };
-
-
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-
   return (
     <html lang="en">
-      <body
-        className="antialiased"
-      >
+      <body className="antialiased">
+        {/* Добавляем мета-тег Google Site Verification */}
+        <Head>
+          <meta
+            name="google-site-verification"
+            content="IuVZJ9aqQSfGVSkVaXr0DWkPRXTVKxF3p0Ay5XOH9kg"
+          />
+        </Head>
+        
         <StoreProvider>
           <ModalProvider>
             <DetailsInfoProvider>
-              <NavBar/>
-                {children}
-              <Footer/>
+              <NavBar />
+              {children}
+              <Footer />
             </DetailsInfoProvider>
           </ModalProvider>
         </StoreProvider>
