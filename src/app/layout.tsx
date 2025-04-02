40cf4f0ded61a43cd1c98765d605f1a03d3ce42d
Miroslav <mirsichkar@gmail.com>
2025-04-02 10:37:45 +0600

    edit CEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,6 @@ import DetailsInfoProvider from "@/context/CardDetailsInfoProvidre";
 import StoreProvider from "./StoreProvider";
 import NavBar from "@/components/Navbar";
 import Footer from "@/components/Footer";
-import Head from "next/head";
 
 export const metadata: Metadata = {
   title: "anishop",
@@ -27,16 +26,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ru">
-      <body className="antialiased">
-        <Head>
-          <meta property="og:title" content="anishop" />
+        <head>
+          <meta name="google-site-verification" content="1m40ZhpkaOBu8WRGiAlr705V3rbGKKkwx9qFUdNcQTE" />
+          {/* <meta property="og:title" content="anishop" />
           <meta property="og:description" content="Интернет-магазин anishop" />
           <meta property="og:image" content="https://firebasestorage.googleapis.com/v0/b/onlineshop-4f347.appspot.com/o/logo%2Fanishop.png?alt=media&token=edd7237f-7bf7-4742-ad6c-7b11a81014d0" />
-          <meta property="og:url" content="https://anishops.vercel.app/" />
+          <meta property="og:url" content="https://anishops.vercel.app/" /> */}
           <meta name="keywords" content="anishop, Anishop, интренет магазин, анишоп" />
-          <meta name="google-site-verification" content="1m40ZhpkaOBu8WRGiAlr705V3rbGKKkwx9qFUdNcQTE" />
-          
-        </Head>
+        </head>
+      <body className="antialiased">
 
         <StoreProvider>
           <ModalProvider>
